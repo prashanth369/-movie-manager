fcf1655f971c10df3cae91c139b5cc3f30321c46
Prashanth Reddy <prashanth.abc369@gmail.com>
2022-01-29 15:34:03 -0500

    Moved create data into seed and added instructions for running the app

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,83 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        $categories_data = array(
+            [
+                'id' => 1,
+                'name' => 'Action',
+                'slug' => 'action'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Animation',
+                'slug' => 'animation'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Comedy',
+                'slug' => 'comedy'
+            ],
+            [
+                'id' => 4,
+                'name' => 'Crime',
+                'slug' => 'crime'
+            ],
+            [
+                'id' => 5,
+                'name' => 'Drama',
+                'slug' => 'drama'
+            ],
+            [
+                'id' => 6,
+                'name' => 'Experimental',
+                'slug' => 'experimental'
+            ],
+            [
+                'id' => 7,
+                'name' => 'Fantasy',
+                'slug' => 'fantasy'
+            ],
+            [
+                'id' => 8,
+                'name' => 'Historical',
+                'slug' => 'historical'
+            ],
+            [
+                'id' => 9,
+                'name' => 'Horror',
+                'slug' => 'horror'
+            ],
+            [
+                'id' => 10,
+                'name' => 'Romance',
+                'slug' => 'romance'
+            ],
+            [
+                'id' => 11,
+                'name' => 'Science Fiction',
+                'slug' => 'science_fiction'
+            ],
+            [
+                'id' => 12,
+                'name' => 'Thriller',
+                'slug' => 'thriller'
+            ],
+            [
+                'id' => 13,
+                'name' => 'Western',
+                'slug' => 'western'
+            ],
+            [
+                'id' => 14,
+                'name' => 'Other',
+                'slug' => 'other'
+            ]
+        );
+
+        foreach($categories_data as $data) {
+            $category = new Category($data);
+            $category->save();
+        }
     }
 }
